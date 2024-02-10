c8486caf416914245b700024b7e3c35def9a52a8
Palamarsha <149620290+Palamarsha@users.noreply.github.com>
2024-02-10 23:06:35 +0200

    ready task -3

--- js/task-2.js
+++ js/task-2.js
@@ -32,4 +32,4 @@ const allUsers = [
 
 console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
 console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); 
